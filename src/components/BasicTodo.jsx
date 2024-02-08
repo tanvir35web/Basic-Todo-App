1e9d4e48d47550c642d51df3e1a64f1aabdc8baa
tanvir35web <islammdtanvirul@gmail.com>
2024-02-08 10:55:38 +0600

    loading prevent

--- src/components/BasicTodo.jsx
+++ src/components/BasicTodo.jsx
@@ -1,88 +1,86 @@
 import React, { useState, useEffect, useRef } from "react";
 import { MdOutlineEditNote } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
-import './BasicTodoResponsive.css'
-
+import "./BasicTodoResponsive.css";
+import { motion } from "framer-motion"            
 
 const BasicTodo = () => {
-  const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState("");
-  const inputRef = useRef(null);
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState("");
+    const inputRef = useRef(null);
 
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+        if (storedTasks) {
+            setTasks(storedTasks);
+        }
+        inputRef.current.focus();
+    }, []);
 
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
-  useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    if (storedTasks) {
-      setTasks(storedTasks);
+    function handleAddTask() {
+        if (input !== "") {
+            setTasks([...tasks, input]);
+            setInput("");
+        }
     }
-    inputRef.current.focus();
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
-
-
-
-  function handleAddTask() {
-    if (input !== "") {
-      setTasks([...tasks, input]);
-      setInput("");
-      
+    function handleDeleteTask(index) {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
     }
-  }
 
-  function handleDeleteTask(index) {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
-  }
-
-  function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      handleAddTask();
+    function handleKeyPress(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+            handleAddTask();
+        }
     }
-  }
 
-  return (
-    <>
-      <div className="container">
-        <h1>
-          {" "}
-          <MdOutlineEditNote size={50} /> Todo App{" "}
-        </h1>
+    return (
+        <>
+            <div className="container">
+                <h1>
+                    {" "}
+                    <MdOutlineEditNote size={50} /> Todo App{" "}
+                </h1>
 
-        <div className="add">
-          <form className="formInput">
-            <input
-              type="text"
-              placeholder="add task here . . ."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyPress={handleKeyPress}
-              ref={inputRef}
+                <div className="add">
+                    <form className="formInput">
+                        <input
+                            type="text"
+                            placeholder="add task here . . ."
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyPress={handleKeyPress}
+                            ref={inputRef}
+                        />
+                        <button type="button" onClick={handleAddTask}>
+                            Add
+                        </button>
+                    </form>
 
-            />
-            <button type="button" onClick={handleAddTask}>
-              Add
-            </button>
-          </form>
-
-          <ul>
-            {tasks.map((task, index) => (
-              <li className="taskList" key={index}>
-                {task}{" "}
-                <div type="button" onClick={() => handleDeleteTask(index)}>
-                  <MdDelete />
+                    <ul>
+                        {tasks.map((task, index) => (
+                            <li className="taskList" key={index}>
+                                {task}{" "}
+                                <div
+                                    type="button"
+                                    onClick={() => handleDeleteTask(index)}
+                                >
+                                    <MdDelete />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 };
 
 export default BasicTodo;
